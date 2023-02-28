94db8d8c801e9f6cdba21f6dee8f55244fa00e02
Rerison <rerison-olivo@hotmail.com>
2023-02-28 19:16:11 -0300

    First Commit!

---
+++ Entidades/Calcular.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Entidades
+{
+    internal class Calcular
+    {
+
+        public double Somar(string valor1, string valor2)
+        {
+            return Convert.ToDouble(valor1) + Convert.ToDouble(valor2);
+        }
+        public double Subtrair(string valor1, string valor2)
+        {
+            return Convert.ToDouble(valor1) - Convert.ToDouble(valor2);
+        }
+        public double Divisao(string valor1, string valor2)
+        { 
+            return Convert.ToDouble(valor1) / Convert.ToDouble(valor2);
+        }
+        public double Multiplicacao(string valor1, string valor2)
+        {
+            return Convert.ToDouble(valor1) * Convert.ToDouble(valor2);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Calculadora.Entidades;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "[";
+            var calcular = new Calcular();
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("------------------CALCULADORA------------------");
+                Console.WriteLine("-----------ESCOLHA A OPCAO QUE DESEJA-----------");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("******************************************");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("");
+                Console.WriteLine("|1| - Somar");
+                Console.WriteLine("|2| - Subtrair");
+                Console.WriteLine("|3| - Divisao");
+                Console.WriteLine("|4| - Multiplicacao");
+                Console.WriteLine("|5| - Encerrar a calculadora");
+                opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1": 
+                        Console.WriteLine("Digite o primeiro numero e logo em seguida o segundo");
+                        Console.WriteLine("Resultado: " + calcular.Somar(Console.ReadLine(), Console.ReadLine()));
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Digite o primeiro numero e logo em seguida o segundo");
+                        Console.WriteLine("Resultado: " + calcular.Subtrair(Console.ReadLine(), Console.ReadLine()));
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Digite o primeiro numero e logo em seguida o segundo");
+                        Console.WriteLine("Resultado: " + calcular.Divisao(Console.ReadLine(), Console.ReadLine()));
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Digite o primeiro numero e logo em seguida o segundo");
+                        Console.WriteLine("Resultado: " + calcular.Multiplicacao(Console.ReadLine(), Console.ReadLine()));
+                        break;
+
+                    default: Console.WriteLine("Opcao errada!"); break;
+                }
+                Console.WriteLine("APERTE ENTER PARA CONTINUAR");
+                Console.ReadLine();
+                Console.Clear();
+            } while (opcao != "5");
+        }
+    }
+}
